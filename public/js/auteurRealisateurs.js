4270172abe721e26a97cf82ec78eca15c315d0a9
thiernoBarry1 <th.barry@yahoo.com>
2019-05-02 16:56:05 +0200

    resolution du problème d'ajout d'auteur et/ou realisateur

--- public/js/auteurRealisateurs.js
+++ public/js/auteurRealisateurs.js
@@ -1,11 +1,10 @@
  // gerer l'ajout d'un auteur ou/et d'un réalisateur lors qu'on clique sur le button
  $('#ajoutAuteur-realisateur').click(function(){
-     console.log('test');
     // je recupère le numero des futurs champs!
     const index  = $('#widget-counter').val();
-
-    const tmpl = $('#registration_auteurRealisateurs').data('prototype');
-    console.log(tmpl);
-    
+    // je remplace tous les __name__ par ce numero
+    const templates = $('#auteurRealisateurs').data('prototype').replace(/__name__/g,index);
+    // j'injecte ce code au sein de la div 
+    $('#auteurRealisateurs').append(templates);
  });
  
